7f6e7524f4750291ff6f296365ae06b749758c1f
Jovany Leandro G.C <bit4bit@riseup.net>
2015-05-14 20:08:12 -0500

    Fix #5

--- skel/android-project/jni/mruby/Android.mk
+++ skel/android-project/jni/mruby/Android.mk
@@ -5,4 +5,7 @@ MRUBY_PATH = ../../../mruby
 LOCAL_MODULE    := mruby-prebuilt
 LOCAL_SRC_FILES := $(MRUBY_PATH)/build/androideabi/lib/libmruby.a
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/$(MRUBY_PATH)/include
+LOCAL_STATIC_LIBRARIES += android_support
+
 include $(PREBUILT_STATIC_LIBRARY)
+$(call import-module,android/support)
